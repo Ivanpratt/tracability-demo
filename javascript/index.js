1d87f3208d335a35a53a05f4d81bcef1c20d8350
Ivan Pratt <ivandoublejr@gmail.com>
2022-03-17 14:41:59 -0600

    button fix 3

--- javascript/index.js
+++ javascript/index.js
@@ -1,18 +1,11 @@
-const express = require('express')
-const cors = require('cors')
-const path = require('path')
-
-const app = express();
-app.use(cors());
-app.use(express.json())
-
-
 
 console.log("Jeddy")
 
 let button = document.getElementsById('button')
 
-button.addEventListener('click', () => {
-    console.log("Jeddy")
 
-})
+let myfunction = function() {
+    console.log("Jeddy")
+}
+
+button.addEventListener('click', myfunction)
