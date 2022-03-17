aefd242496774cbdbd237b210023a19a7c29fea1
Ivan Pratt <ivandoublejr@gmail.com>
2022-03-17 15:42:22 -0600

    get axios

--- javascript/index.js
+++ javascript/index.js
@@ -2,15 +2,6 @@
 console.log("Jeddy")
 
 
-const express = require('express')
-const cors = require('cors')
-const path = require('path')
-
-const app = express();
-app.use(cors());
-app.use(express.json())
-
-
 let button = document.getElementById('button')
 
 
