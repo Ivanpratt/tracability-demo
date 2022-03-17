0f37c2feee900569826f06053f2b0d0821c8ab10
Ivan Pratt <ivandoublejr@gmail.com>
2022-03-17 14:47:58 -0600

    added rollbar attempt

--- javascript/index.js
+++ javascript/index.js
@@ -1,6 +1,19 @@
 
 console.log("Jeddy")
 
+
+// include and initialize the rollbar library with your access token
+var Rollbar = require("rollbar");
+var rollbar = new Rollbar({
+  accessToken: 'f56574e5d5f8427f8eeaced774a70f48',
+  captureUncaught: true,
+  captureUnhandledRejections: true
+});
+
+// record a generic message and send it to Rollbar
+rollbar.log("Hello world!");
+
+
 let button = document.getElementById('button')
 
 
